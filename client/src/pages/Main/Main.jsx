7b6d27c21b64764ee2d7fbd43660798cc8b69b62
Skaidre Petraviciene <skaidreju@gmail.com>
2023-07-10 23:17:39 +0300

    Main ir stiliai, bet neveikia

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -1,9 +1,13 @@
+import Topbar from "../../components/Topbar/Topbar";
+import Footer from "../../components/Footer";
 import Image from "../../images/plep.png";
 
 const Main = () => {
   return (
     <main>
+      <Topbar />
       <img className="bg-img" src={Image} alt="" />
+      <Footer />
     </main>
   );
 };
