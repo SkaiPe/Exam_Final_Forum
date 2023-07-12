5aee7f7a1ce4f389fcab03b6fd77cf19c944fb3b
SkaiPe <skaidreju@gmail.com>
2023-07-12 22:25:32 +0300

    Sukurtas klausimo uždavimo puslapis

---
+++ client/src/pages/Question/Question.jsx
@@ -0,0 +1,46 @@
+import { useContext, useState } from "react";
+import Input from "../../components/Input";
+import axios from "axios";
+import { UserContext } from "../../context/UserContext";
+
+const Question = () => {
+  const [question, setQuestion] = useState("");
+  const { user } = useContext(UserContext);
+
+  const questionOnChange = (e) => {
+    setQuestion(e.target.value);
+  };
+
+  const userQuestion = {
+    text: question,
+    edited: false,
+    date: new Date().toLocaleString(),
+    name: user.name,
+  };
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3000/question", userQuestion)
+      .then(() => {
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  return (
+    <form onSubmit={formSubmit}>
+      <h1>Užduokite klausimą</h1>
+      <Input
+        value={question}
+        onChange={questionOnChange}
+        placeholder="Užduokite klausimą..."
+      />
+      <button type="submit">Klausti</button>
+    </form>
+  );
+};
+
+export default Question;
